bef09d97149582d4474a50ece8f4272d1f54e614
JakubKozacek <jakubkozacek@gmail.com>
2014-04-30 19:43:57 +0200

    Create init.h

---
+++ init.h
@@ -0,0 +1,11 @@
+
+void init(void) {
+  puts("............................ [  ");
+  textcolor(LIGHTGREEN);
+  puts("OK");
+  textcolor(LIGHTGRAY);
+  puts("  ]\r\n\n");
+  puts("Startujem SHELL...\r\n\n");
+  
+  return;
+}
