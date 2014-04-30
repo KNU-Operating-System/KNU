609355a474c56d86b9d02a3f1d45c44eb98e3cab
JakubKozacek <jakubkozacek@gmail.com>
2014-04-30 19:42:39 +0200

    Create global.h

---
+++ global.h
@@ -0,0 +1,14 @@
+#ifndef _GLOBAL_INCLUDED_
+#define _GLOBAL_INCLUDED_
+/*
+ *  Tu sa môžu vyskitovať globálne premenné 
+ *  celého kernelu...
+ *  
+ */
+
+
+/* TEXT_COL, globálna premenná určujúca farbu textu */
+
+char TEXT_COL = 0x07;
+
+#endif
